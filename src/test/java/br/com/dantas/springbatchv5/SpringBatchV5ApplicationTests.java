dc789ab63913ebb5f337381d904d174acbf512af
Bruno Dantas <brunoodantas7@gmail.com>
2024-04-01 14:16:16 -0300

    first commit init repo

---
+++ src/test/java/br/com/dantas/springbatchv5/SpringBatchV5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dantas.springbatchv5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchV5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
