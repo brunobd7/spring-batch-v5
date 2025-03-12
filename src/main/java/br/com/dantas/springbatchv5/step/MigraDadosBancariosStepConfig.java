9ff3ae58527c72ad358dd4cd6a9c3393c0948810
Bruno Dantas <brunoodantas7@gmail.com>
2025-03-12 19:35:38 -0300

    add: Steps config to data migration proccessment.

---
+++ src/main/java/br/com/dantas/springbatchv5/step/MigraDadosBancariosStepConfig.java
@@ -0,0 +1,31 @@
+package br.com.dantas.springbatchv5.step;
+
+import br.com.dantas.springbatchv5.domain.DadosBancarios;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class MigraDadosBancariosStepConfig {
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager platformTransactionManager;
+
+    public MigraDadosBancariosStepConfig(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+        this.jobRepository = jobRepository;
+        this.platformTransactionManager = platformTransactionManager;
+    }
+
+    @Bean
+    public Step migraDadosBancariosStep(ItemReader<DadosBancarios> reader, ItemWriter<DadosBancarios> writer){
+        return new StepBuilder("migraDadosBancariosStep",jobRepository)
+                .<DadosBancarios,DadosBancarios>chunk(1,platformTransactionManager)
+                .build();
+    }
+
+}
